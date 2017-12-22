322db6202ff1b78aa05607f839c8107e57858617
ShayPatrickCormac <574583006@qq.com>
2017-12-22 23:35:58 +0800

    使用IntelliJIdea

---
+++ WebRoot/js/shay1.js
@@ -0,0 +1,19 @@
+/**
+ * Created by ShayPatrickCormac on 2017/12/22.
+ */
+alert("我操，碉堡了啊！！！！");
+
+/**
+ * 打印指定参数数据到页面上，并换行。
+ */
+function printn(param) {
+    document.write(param+"</br>");
+
+}
+/**
+ * 打印指定参数数据到页面上。
+ * @param param
+ */
+function print(param) {
+    document.write(param);
+}
